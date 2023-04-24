7a425f6da53fb0da57c658199de4f423f4dde650
KazmaXND <blues6163853@gmail.com>
2023-04-25 01:39:19 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace OOXX
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.startGame = new System.Windows.Forms.Button();
+			this.button_00 = new System.Windows.Forms.Button();
+			this.button_01 = new System.Windows.Forms.Button();
+			this.button_02 = new System.Windows.Forms.Button();
+			this.button_10 = new System.Windows.Forms.Button();
+			this.button_11 = new System.Windows.Forms.Button();
+			this.button_12 = new System.Windows.Forms.Button();
+			this.button_20 = new System.Windows.Forms.Button();
+			this.button_21 = new System.Windows.Forms.Button();
+			this.button_22 = new System.Windows.Forms.Button();
+			this.StopApp = new System.Windows.Forms.Button();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(254, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(168, 47);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "井字遊戲";
+			// 
+			// startGame
+			// 
+			this.startGame.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.startGame.Location = new System.Drawing.Point(182, 75);
+			this.startGame.Name = "startGame";
+			this.startGame.Size = new System.Drawing.Size(210, 37);
+			this.startGame.TabIndex = 1;
+			this.startGame.Text = "開始遊戲";
+			this.startGame.UseVisualStyleBackColor = true;
+			this.startGame.Click += new System.EventHandler(this.startGame_Click);
+			// 
+			// button_00
+			// 
+			this.button_00.Location = new System.Drawing.Point(182, 126);
+			this.button_00.Name = "button_00";
+			this.button_00.Size = new System.Drawing.Size(100, 100);
+			this.button_00.TabIndex = 2;
+			this.button_00.UseVisualStyleBackColor = true;
+			this.button_00.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_01
+			// 
+			this.button_01.Location = new System.Drawing.Point(288, 126);
+			this.button_01.Name = "button_01";
+			this.button_01.Size = new System.Drawing.Size(100, 100);
+			this.button_01.TabIndex = 2;
+			this.button_01.UseVisualStyleBackColor = true;
+			this.button_01.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_02
+			// 
+			this.button_02.Location = new System.Drawing.Point(394, 126);
+			this.button_02.Name = "button_02";
+			this.button_02.Size = new System.Drawing.Size(100, 100);
+			this.button_02.TabIndex = 2;
+			this.button_02.UseVisualStyleBackColor = true;
+			this.button_02.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_10
+			// 
+			this.button_10.Location = new System.Drawing.Point(182, 232);
+			this.button_10.Name = "button_10";
+			this.button_10.Size = new System.Drawing.Size(100, 100);
+			this.button_10.TabIndex = 2;
+			this.button_10.UseVisualStyleBackColor = true;
+			this.button_10.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_11
+			// 
+			this.button_11.Location = new System.Drawing.Point(288, 232);
+			this.button_11.Name = "button_11";
+			this.button_11.Size = new System.Drawing.Size(100, 100);
+			this.button_11.TabIndex = 2;
+			this.button_11.UseVisualStyleBackColor = true;
+			this.button_11.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_12
+			// 
+			this.button_12.Location = new System.Drawing.Point(394, 232);
+			this.button_12.Name = "button_12";
+			this.button_12.Size = new System.Drawing.Size(100, 100);
+			this.button_12.TabIndex = 2;
+			this.button_12.UseVisualStyleBackColor = true;
+			this.button_12.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_20
+			// 
+			this.button_20.Location = new System.Drawing.Point(182, 338);
+			this.button_20.Name = "button_20";
+			this.button_20.Size = new System.Drawing.Size(100, 100);
+			this.button_20.TabIndex = 2;
+			this.button_20.UseVisualStyleBackColor = true;
+			this.button_20.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_21
+			// 
+			this.button_21.Location = new System.Drawing.Point(288, 338);
+			this.button_21.Name = "button_21";
+			this.button_21.Size = new System.Drawing.Size(100, 100);
+			this.button_21.TabIndex = 2;
+			this.button_21.UseVisualStyleBackColor = true;
+			this.button_21.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// button_22
+			// 
+			this.button_22.Location = new System.Drawing.Point(394, 338);
+			this.button_22.Name = "button_22";
+			this.button_22.Size = new System.Drawing.Size(100, 100);
+			this.button_22.TabIndex = 2;
+			this.button_22.UseVisualStyleBackColor = true;
+			this.button_22.Click += new System.EventHandler(this.GameButton_Click);
+			// 
+			// StopApp
+			// 
+			this.StopApp.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.StopApp.Location = new System.Drawing.Point(229, 453);
+			this.StopApp.Name = "StopApp";
+			this.StopApp.Size = new System.Drawing.Size(210, 37);
+			this.StopApp.TabIndex = 1;
+			this.StopApp.Text = "關閉";
+			this.StopApp.UseVisualStyleBackColor = true;
+			this.StopApp.Click += new System.EventHandler(this.StopApp_Click);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.radioButton1.Location = new System.Drawing.Point(409, 75);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(71, 16);
+			this.radioButton1.TabIndex = 3;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "玩家先手";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(409, 96);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(71, 16);
+			this.radioButton2.TabIndex = 3;
+			this.radioButton2.Text = "電腦先手";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 518);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.button_22);
+			this.Controls.Add(this.button_21);
+			this.Controls.Add(this.button_12);
+			this.Controls.Add(this.button_11);
+			this.Controls.Add(this.button_20);
+			this.Controls.Add(this.button_02);
+			this.Controls.Add(this.button_10);
+			this.Controls.Add(this.button_01);
+			this.Controls.Add(this.button_00);
+			this.Controls.Add(this.StopApp);
+			this.Controls.Add(this.startGame);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button startGame;
+		private System.Windows.Forms.Button button_00;
+		private System.Windows.Forms.Button button_01;
+		private System.Windows.Forms.Button button_02;
+		private System.Windows.Forms.Button button_10;
+		private System.Windows.Forms.Button button_11;
+		private System.Windows.Forms.Button button_12;
+		private System.Windows.Forms.Button button_20;
+		private System.Windows.Forms.Button button_21;
+		private System.Windows.Forms.Button button_22;
+		private System.Windows.Forms.Button StopApp;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.XPath;
+
+namespace OOXX
+{
+	public partial class Form1 : Form
+	{
+		int[,] arr = new int[3, 3];
+		int spacePos = -1; // 定義空位置
+		string codePath = (AppDomain.CurrentDomain.BaseDirectory) + "..\\..\\";
+		List<Button> buttonList = new List<Button>();
+		Image xPath, oPath;
+		Image npc_img, player_img;
+		public Form1()
+		{
+			InitializeComponent();
+			//MessageBox.Show(AppDomain.CurrentDomain.BaseDirectory);
+			buttonList.Add(button_00);
+			buttonList.Add(button_01);
+			buttonList.Add(button_02);
+			buttonList.Add(button_10);
+			buttonList.Add(button_11);
+			buttonList.Add(button_12);
+			buttonList.Add(button_20);
+			buttonList.Add(button_21);
+			buttonList.Add(button_22);
+            foreach (var item in buttonList)
+            {
+				item.Enabled = false;
+            }
+            //CheckPathExists();
+            xPath = Image.FromFile(codePath + "Img/img_X.jpg");
+			oPath = Image.FromFile(codePath + "Img/img_O.jpg");
+			CheckPathExists();
+			npc_img = oPath;
+			player_img = xPath;
+		}
+
+		private void startGame_Click(object sender, EventArgs e)
+		{
+			foreach (var btn in buttonList)
+			{
+				btn.Image = null;
+				btn.Enabled = true;
+				for (int i = 0; i < 3; i++)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						arr[i, j] = spacePos;
+					}
+				}
+			}
+			if (radioButton2.Checked)
+			{
+				NpcPlay();
+			}
+		}
+
+		private void StopApp_Click(object sender, EventArgs e)
+		{
+			// stop application
+			Process.GetCurrentProcess().Kill();
+		}
+
+		private void GameButton_Click(object sender, EventArgs e)
+		{
+			Button btn = (Button)sender;
+			// let player_img size fit button size
+			player_img = new Bitmap(player_img, btn.Width, btn.Height);
+			btn.Image = player_img;
+			btn.Enabled = false;
+			string pos = btn.Name.Substring(btn.Name.Length - 2, 2);
+			Debug.WriteLine("pos index: " + pos);
+			// 設定玩家參數為1
+			arr[Convert.ToInt32(pos[0] - '0'), Convert.ToInt32(pos[1] - '0')] = 1;
+			CheckWin(true);
+			// 電腦下棋
+			NpcPlay();
+			CheckWin(false);
+		}
+		private bool NpcPlay()
+		{
+			if (arr[1,1] == spacePos)
+			{
+				NpcAction(1, 1);
+				return true;
+			}
+			// 尋找電腦快勝利的格子
+			for(int i  = 0; i < 3; i++)
+			{
+				int chkrow = 0;
+				if (arr[0, i] + arr[1, i] + arr[2, i] == 3)
+				{
+					for (int j = i; j < 3; j++)
+					{
+						if (arr[j, i] == spacePos)
+						{
+							NpcAction(j, i);
+							return true;
+						}
+					}
+				}
+				for (int j = 0; j < 3; j++)
+				{
+					if (arr[i, j] == 2)
+						chkrow++;
+				}
+				if (chkrow == 2)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						if (arr[i, j] == spacePos)
+						{
+							NpcAction(i, j);
+							return true;
+						}
+					}
+				}
+			}
+			if (arr[0, 0] + arr[1, 1] + arr[2, 2] == 3)
+			{
+				for (int i = 0; i < 3; i++)
+					if (arr[i, i] == spacePos)
+						NpcAction(i, i);
+				return true;
+			}
+			if (arr[2, 0] + arr[1, 1] + arr[0, 2] == 3)
+			{
+				for (int i = 0; i < 3; i++)
+					if (arr[2 - i, i] == spacePos)
+						NpcAction(2-i, i);
+				return true;
+			}
+			// 尋找玩家快勝利的格子
+			for (int i = 0; i < 3; i++)
+			{
+				int chkrow = 0;
+				if (arr[0, i] + arr[1, i] + arr[2, i] == 1)
+				{
+					for(int j = 0; j< 3; j++)
+					{
+						if (arr[j, i] == spacePos)
+						{
+							NpcAction(j, i);
+							return true;
+						}
+					}
+				}
+				for (int j = 0; j < 3; j++)
+				{
+					if (arr[i, j] == 1)
+						chkrow++;
+				}
+				if(chkrow == 2)
+				{
+					for( int j = 0; j< 3; j++)
+					{
+						if (arr[i, j] == spacePos)
+						{
+							NpcAction(i, j);
+							return true;
+						}
+					}
+				}
+			}
+			if (arr[0, 0] + arr[1, 1] + arr[2, 2] == 1)
+			{
+				for (int i = 0; i < 3; i++)
+					if (arr[i, i] == spacePos)
+						NpcAction(i, i);
+				return true;
+			}
+			if (arr[2, 0] + arr[1, 1] + arr[0, 2] == 1)
+			{
+				for (int i = 0; i < 3; i++)
+					if (arr[2 - i, i] == spacePos)
+						NpcAction(2-i, i);
+				return true;
+			}
+
+			for (int i = 0; i < 3; i+=2)
+			{
+				for(int j = 0; j < 3; j += 2)
+				{
+					if (arr[i, j] == spacePos)
+					{
+						NpcAction(i, j);
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+
+		// 設定電腦參數為2
+		private void NpcAction(int row, int col)
+		{
+			arr[row, col] = 2;
+			buttonList[row * 3 + col].Image = npc_img;
+			buttonList[row * 3 + col].Enabled = false;
+		}
+
+		private bool CheckWin(bool player)
+		{
+			bool playerWinFlag = false, npcWinFlag = false;
+			if(player)
+				playerWinFlag = ChkLine(true);
+			else npcWinFlag = ChkLine(false);
+			
+			if (playerWinFlag || npcWinFlag)
+			{
+				MessageBox.Show(playerWinFlag ? "玩家勝利" : "電腦勝利");
+				foreach (var item in buttonList)
+				{
+					item.Enabled = false;
+				}
+			}
+			return false;
+		}
+
+		private bool ChkLine(bool player)
+		{
+			int chkNum = player ? 3 : 6;
+			for (int i = 0; i < 3; i++)
+			{
+				if (arr[i, 0] + arr[i, 1] + arr[i, 2] == chkNum)
+				{
+					for(int j = 0; j < 3; j++)
+					{
+						if(arr[i, j] != (player ? 1: 2))
+						{
+							return false;
+						}
+					}
+					return true;
+				}
+				if (arr[0, i] + arr[1, i] + arr[2, i] == chkNum)
+				{
+					for(int j=0; j < 3; j++)
+					{
+						if (arr[j, i] != (player ? 1: 2))
+						{
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			if (arr[0, 0] + arr[1, 1] + arr[2, 2] == chkNum)
+			{
+				for (int i = 0; i < 3; i++)
+					if (arr[i, i] != (player ? 1:2))
+						return false;
+				return true;
+			}
+			if (arr[2, 0] + arr[1, 1] + arr[0, 2] == chkNum)
+			{
+				for(int i = 0; i< 3; i++)
+					if (arr[2 - i, i] != (player ? 1 : 2))
+						return false;
+				return true;
+			}
+			return false;
+		}
+
+		private void CheckPathExists()
+		{
+			// check the path excist or not
+			if (!System.IO.File.Exists(codePath + "Img/img_X.jpg"))
+			{
+				MessageBox.Show("img_X.jpg not exist");
+			}
+			if (!System.IO.File.Exists(codePath + "Img/img_O.jpg"))
+			{
+				MessageBox.Show("img_O.jpg not exist");
+			}
+		}
+	}
+}
